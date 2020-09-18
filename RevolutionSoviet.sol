721a56007d1ad6ee8403293180cb88c13687b21f
soviet <kang.yan@expread.io>
2020-09-18 14:56:22 +0800

    init

---
+++ RevolutionSoviet.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.6.2;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+// rSOV token with Governance.
+contract RevolutionSoviet is ERC20("Revolution Soviet", "rSOV"), Ownable {
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (Uprising).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
